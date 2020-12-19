4e79d55b6f3e3df4133abbc3055ecb88392d413a
WilliamRoll <broll6595@gmail.com>
2020-12-18 19:48:50 -0500

    UFO sightings data is correctly displaying as a table within the web page

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -6,3 +6,17 @@ var tbody = d3.select("tbody");
 
 //Console.log the UFO sightings data from data.js
 console.log(data);
+
+//Loop through `data` and console.log each UFO sighting
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+  });
+
+//Update each cell's text with UFO sightings data
+data.forEach((ufoSighting) => {
+var row = tbody.append("tr");
+Object.entries(ufoSighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+});
+});
